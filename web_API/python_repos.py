a9672b74ff70ee3644801129d23d109029ad1f78
zhaoman-PC <zhaomancse@qq.com>
2019-10-07 16:18:07 +0800

    2019-10-07

--- web_API/python_repos.py
+++ web_API/python_repos.py
@@ -1,21 +1,24 @@
 import requests
+from plotly.graph_objs import Bar
+from plotly import offline
 
-# Make an API call and store the response.
+# 执行API调用
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
-headers = {'Accept': 'application/vnd.github.v3+json'}
-r = requests.get(url, headers=headers)
-print(f"Status code: {r.status_code}")
-
-# Store API response in a variable.
+r = requests.get(url)
+# 核实调用是否成功，状态码200成功
+print("Status code: ", r.status_code)
+# 转化成json的字典形式
 response_dict = r.json()
-print(f"Total repositories: {response_dict['total_count']}")
+print(response_dict.keys())
+print("Total repositories: ", response_dict['total_count'])
 
-# Explore information about the repositories.
+# 探索有关仓库的信息
 repo_dicts = response_dict['items']
-print(f"Repositories returned: {len(repo_dicts)}")
+print("Repositories returned: ", len(repo_dicts))
 
-print("\nSelected information about each repository:")
+
 for repo_dict in repo_dicts:
+    print("-------------------------")
     print(f"Name: {repo_dict['name']}")
     print(f"Owner: {repo_dict['owner']['login']}")
     print(f"Stars: {repo_dict['stargazers_count']}")
@@ -23,3 +26,51 @@ for repo_dict in repo_dicts:
     print(f"Created: {repo_dict['created_at']}")
     print(f"Updated: {repo_dict['updated_at']}")
     print(f"Description: {repo_dict['description']}")
+
+
+repo_links, stars, labels = [], [], []
+for repo_dict in repo_dicts:
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
+
+    stars.append(repo_dict['stargazers_count'])
+
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    label = f"{owner}<br />{description}"
+    labels.append(label)
+
+
+# bar的统计可视化
+data = [{
+    'type': 'bar',
+    'x': repo_links,
+    'y': stars,
+    'hovertext': labels,
+    'marker': {
+        'color': 'rgb(60, 100, 150)',
+        'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
+    },
+    'opacity': 0.6,
+}]
+
+my_layout = {
+    'title': 'Most-Starred Python Projects on GitHub',
+    'titlefont': {'size': 28},
+    'xaxis': {
+        'title': 'Repository',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},
+    },
+    'yaxis': {
+        'title': 'Stars',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},
+    },
+
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='python_repos.html')
